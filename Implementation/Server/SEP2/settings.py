ff591b49078c40d6766045c5dbac97a1b0f813b8
Jinsan Kim <jinsan.dev@gmail.com>
2021-10-08 16:26:28 +0900

    Update settings.py

--- Implementation/Server/SEP2/settings.py
+++ Implementation/Server/SEP2/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-d7(hir-0s5yf=lrui6%s1e8ud#1e=_67wwnl93a-69f&9ekl09'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
